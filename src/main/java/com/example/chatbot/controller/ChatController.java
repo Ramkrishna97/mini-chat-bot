02de046d121b280d27b137feafb3d42e600434d9
Ramkrishna97 <ramkrishnagoswami10@gmail.com>
2026-03-08 14:00:03 +0530

    Adding project files, updating existing files.

--- src/main/java/com/example/chatbot/controller/ChatController.java
+++ src/main/java/com/example/chatbot/controller/ChatController.java
@@ -5,10 +5,5 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
-//
-//    // Alternative: GET with query parameter
-//    @GetMapping("/ask")
-//    public String askQuestion(@RequestParam String question) {
-//        return llmService.processQuery(question);
 //    }
 }
