ef83e0e06076c2f738adc8ccb5d422adf92783b1
Ramkrishna97 <ramkrishnagoswami10@gmail.com>
2026-03-09 09:06:16 +0530

    Adding project files, updating existing files.

--- src/main/java/com/example/chatbot/config/SwaggerConfig.java
+++ src/main/java/com/example/chatbot/config/SwaggerConfig.java
@@ -1,9 +1,5 @@
 package com.example.chatbot.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.Contact;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
